da3ec90275830f0cde645563d82705882cbf4b44
Gaurav Sarma <gaurav4sarma@gmail.com>
2022-08-04 00:02:29 +0530

    Refactored code

--- androidApp/src/main/java/io/ballerine/kmp/example/android/BallerineWebView.kt
+++ androidApp/src/main/java/io/ballerine/kmp/example/android/BallerineWebView.kt
@@ -1,2 +1,110 @@
 package io.ballerine.kmp.example.android
 
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.util.Log
+import android.view.ViewGroup
+import android.webkit.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import java.io.File
+import java.util.concurrent.ExecutorService
+
+/**
+ * Custom Web view which handles the web verification flow
+ */
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun BallerineWebView(
+    outputFileDirectory: File,
+    cameraExecutorService: ExecutorService,
+    url: String,
+    onVerificationComplete: (String) -> Unit,
+) {
+
+    var isOpenFrontCamera by remember {
+        mutableStateOf(false)
+    }
+    var filePathCallback: ValueCallback<Array<Uri>>? by remember {
+        mutableStateOf(null)
+    }
+    var showMediaPicker by remember {
+        mutableStateOf(false)
+    }
+
+    if (showMediaPicker) {
+        CameraView(
+            isOpenFrontCamera = isOpenFrontCamera,
+            outputDirectory = outputFileDirectory,
+            executor = cameraExecutorService,
+            onImageCaptured = { uri ->
+                filePathCallback!!.onReceiveValue(arrayOf(uri))
+                filePathCallback = null
+                showMediaPicker = false
+            }
+        ) { Log.e("CameraView", "View error:", it) }
+    }
+
+    val webViewChromeClient = object : WebChromeClient() {
+        override fun onShowFileChooser(
+            webView: WebView?,
+            filePathCb: ValueCallback<Array<Uri>>?,
+            fileChooserParams: FileChooserParams?,
+        ): Boolean {
+            if (filePathCallback != null) {
+                filePathCallback!!.onReceiveValue(null)
+            }
+            filePathCallback = filePathCb
+            showMediaPicker = true
+            return true
+        }
+
+        override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+            Log.e("onConsoleMessage", " ---> ${consoleMessage?.message()}")
+            return super.onConsoleMessage(consoleMessage)
+        }
+    }
+
+    val webViewClient = object : WebViewClient() {
+
+        override fun doUpdateVisitedHistory(view: WebView?, url: String, isReload: Boolean) {
+            Log.d("doUpdateVisitedHistory", "url -> $url")
+
+            val uri = Uri.parse(url)
+            uri.getQueryParameter("close")?.let { paramValue ->
+                if (paramValue == "true") {
+                    onVerificationComplete(paramValue)
+                }
+            }
+
+            isOpenFrontCamera = url.contains("selfie") == true
+
+            super.doUpdateVisitedHistory(view, url, isReload)
+        }
+    }
+
+    /**
+     * Set the Display parameters to display the view in your Android app screen
+     */
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                this.webViewClient = webViewClient
+                this.webChromeClient = webViewChromeClient
+                this.settings.apply {
+                    javaScriptEnabled = true
+                    domStorageEnabled = true
+                    allowFileAccess = true
+                    allowContentAccess = true
+                }
+                loadUrl(url)
+            }
+        })
+}
--- androidApp/src/main/java/io/ballerine/kmp/example/android/MainActivity.kt
+++ androidApp/src/main/java/io/ballerine/kmp/example/android/MainActivity.kt
@@ -1,12 +1,7 @@
 package io.ballerine.kmp.example.android
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.view.ViewGroup
-import android.webkit.*
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
@@ -18,7 +13,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.viewinterop.AndroidView
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionsRequired
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
@@ -28,19 +22,18 @@ import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var outputDirectory: File
-    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var outputFileDirectory: File
+    private lateinit var cameraExecutorService: ExecutorService
 
 
     @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        outputDirectory = getOutputDirectory()
-        cameraExecutor = Executors.newSingleThreadExecutor()
+        outputFileDirectory = getOutputDirectory()
+        cameraExecutorService = Executors.newSingleThreadExecutor()
 
         setContent {
-
             val permissionState =
                 rememberMultiplePermissionsState(arrayListOf(Manifest.permission.CAMERA))
 
@@ -59,16 +52,6 @@ class MainActivity : AppCompatActivity() {
                     MainScreen()
                 }
             )
-        }
-    }
-
-    @Composable
-    fun AllowCameraAccess() {
-        Column(
-            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = getString(R.string.allow_camera_access))
         }
     }
 
@@ -114,107 +97,27 @@ class MainActivity : AppCompatActivity() {
                 Column(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    CustomWebView(url = "https://vendy.dev.ballerine.app",
-                        onFinish = { finalUrl ->
-
+                    BallerineWebView(
+                        outputFileDirectory,
+                        cameraExecutorService,
+                        url = "https://vendy.dev.ballerine.app",
+                        onVerificationComplete = { result ->
                             currentPage = mainScreen
-
-                            val uri = Uri.parse(finalUrl)
-                            uri.getQueryParameter("close")?.let { queryParam ->
-                                secretValue = queryParam
-                            }
+                            secretValue = result
                         })
                 }
             }
         }
     }
 
-
-    @SuppressLint("SetJavaScriptEnabled")
     @Composable
-    fun CustomWebView(
-        url: String,
-        onFinish: (String) -> Unit
-    ) {
-
-        var isOpenFrontCamera by remember {
-            mutableStateOf(false)
+    fun AllowCameraAccess() {
+        Column(
+            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = getString(R.string.allow_camera_access))
         }
-        var filePathCallback: ValueCallback<Array<Uri>>? by remember {
-            mutableStateOf(null)
-        }
-        var showMediaPicker by remember {
-            mutableStateOf(false)
-        }
-
-        if (showMediaPicker) {
-
-            CameraView(
-                isOpenFrontCamera = isOpenFrontCamera,
-                outputDirectory = outputDirectory,
-                executor = cameraExecutor,
-                onImageCaptured = { uri ->
-                    filePathCallback!!.onReceiveValue(arrayOf(uri))
-                    filePathCallback = null
-                    showMediaPicker = false
-                }
-            ) { Log.e("CameraView", "View error:", it) }
-        }
-
-        val webViewChromeClient = object : WebChromeClient() {
-            override fun onShowFileChooser(
-                webView: WebView?,
-                filePathCb: ValueCallback<Array<Uri>>?,
-                fileChooserParams: FileChooserParams?
-            ): Boolean {
-                if (filePathCallback != null) {
-                    filePathCallback!!.onReceiveValue(null)
-                }
-                filePathCallback = filePathCb
-                showMediaPicker = true
-                return true
-            }
-
-            override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                Log.e("onConsoleMessage", " ---> ${consoleMessage?.message()}")
-                return super.onConsoleMessage(consoleMessage)
-            }
-        }
-
-        val webViewClient = object : WebViewClient() {
-
-            override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
-                Log.d("doUpdateVisitedHistory", "url -> $url")
-
-                if (url?.contains("close") == true) {
-                    onFinish(url)
-                }
-
-                isOpenFrontCamera = url?.contains("selfie") == true
-
-                super.doUpdateVisitedHistory(view, url, isReload)
-            }
-        }
-
-        AndroidView(
-            modifier = Modifier.fillMaxSize(),
-            factory = { ctx ->
-                WebView(ctx).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
-                    this.webViewClient = webViewClient
-                    this.webChromeClient = webViewChromeClient
-                    this.settings.apply {
-                        javaScriptEnabled = true
-                        domStorageEnabled = true
-                        allowFileAccess = true
-                        allowContentAccess = true
-                    }
-                    loadUrl(url)
-                }
-            })
     }
 
     private fun getOutputDirectory(): File {
@@ -226,6 +129,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        cameraExecutor.shutdown()
+        cameraExecutorService.shutdown()
     }
 }
